1e7debff543346057d5a8e32b4447f4a61632eef
Gabriel Krummenacher <gabriel.krummenacher@inf.ethz.ch>
2016-01-30 21:41:22 +0100

    wisdom functions

--- fjlt/random_projection_fast.pyx
+++ fjlt/random_projection_fast.pyx
@@ -31,6 +31,17 @@ cdef extern from "fftw3.h":
         FFTW_RODFT01=8
         FFTW_RODFT10=9
         FFTW_RODFT11=10
+    
+    cdef enum:
+        FFTW_MEASURE = 0
+        FFTW_DESTROY_INPUT = 1
+        FFTW_UNALIGNED = 2
+        FFTW_CONSERVE_MEMORY = 4
+        FFTW_EXHAUSTIVE = 8
+        FFTW_PRESERVE_INPUT = 16
+        FFTW_PATIENT = 32
+        FFTW_ESTIMATE = 64
+        FFTW_WISDOM_ONLY = 2097152
 
     ctypedef fftw_r2r_kind_do_not_use_me fftw_r2r_kind
     fftw_plan fftw_plan_r2r(int rank, const int *n, double *input, double *output, const fftw_r2r_kind *kind, unsigned int flags)
@@ -53,10 +64,19 @@ cpdef import_wisdom(wisdom_file):
         wisdom = wsdf.read()
         fftw_import_wisdom_from_string(wisdom)
 
+
 cpdef export_wisdom(wisdom_file):
     wisdom = fftw_export_wisdom_to_string()
+    print wisdom
     with open(wisdom_file, 'w') as wsdf:
         wsdf.write(wisdom)
+
+def import_wisdom_from_string(const char *input_string):
+    return fftw_import_wisdom_from_string(input_string)
+
+def export_wisdom_to_string():
+    return fftw_export_wisdom_to_string()
+
 
 def precompute_plan_many(np.ndarray[double, ndim=2, mode='c'] X):
     cdef np.int_t n = X.shape[0]
@@ -167,7 +187,6 @@ cpdef double[:] fast_unitary_transform_fast_1d(double[:] X, double[:] D):
     cdef fftw_r2r_kind kind = FFTW_REDFT10
     cdef fftw_plan fftplan = fftw_plan_r2r(1, &dims[0], &plan_arr[0], &plan_arr[0], &kind, 8)
 
-
     fftw_execute_r2r(fftplan, &X[0], &X[0])
 
     X[0] /= sqrt(2)
@@ -176,8 +195,48 @@ cpdef double[:] fast_unitary_transform_fast_1d(double[:] X, double[:] D):
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef float[:] fast_unitary_transform_fast_1d32(float[:] X, float[:] D):
+cpdef fftw_plan fast_unitary_transform_fast_1d32_plan(float[:] X, char* wisdom_file):
+    cdef np.int_t n = X.shape[0]
+    cdef int[:] dims = np.asarray([n], dtype=np.intc)
+#     cdef float[:] plan_arr = np.empty(n, dtype=np.float32)
+    cdef fftw_r2r_kind kind = FFTW_REDFT10
+    export_wisdom(wisdom_file)
+    cdef fftw_plan fftplan = fftwf_plan_r2r(1, &dims[0], &X[0], &X[0], &kind, FFTW_EXHAUSTIVE)
+    print fftplan
+    
+    export_wisdom(wisdom_file)
+    
+    return fftplan
 
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef float[:] fast_unitary_transform_fast_1d32_wsdmonly(float[:] X, float[:] D):
+
+    cdef np.int_t n = X.shape[0]
+
+    cdef float scale = 1 / sqrt(2 * n)
+
+    for i in range(n):
+        X[i] = scale * D[i] * X[i]
+
+    cdef int[:] dims = np.asarray([n], dtype=np.intc)
+#     cdef float[:] plan_arr = np.empty(n, dtype=np.float32)
+    cdef fftw_r2r_kind kind = FFTW_REDFT10
+    cdef fftw_plan fftplan = fftwf_plan_r2r(1, &dims[0], &X[0], &X[0], &kind, FFTW_WISDOM_ONLY)
+
+
+    fftwf_execute_r2r(fftplan, &X[0], &X[0])
+
+    X[0] /= sqrt(2)
+
+    return X
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef float[:] fast_unitary_transform_fast_1d32(float[:] X, float[:] D):
+#     print export_wisdom_to_string()
     cdef np.int_t n = X.shape[0]
 
     cdef float scale = 1 / sqrt(2 * n)
@@ -188,8 +247,9 @@ cpdef float[:] fast_unitary_transform_fast_1d32(float[:] X, float[:] D):
     cdef int[:] dims = np.asarray([n], dtype=np.intc)
     cdef float[:] plan_arr = np.empty(n, dtype=np.float32)
     cdef fftw_r2r_kind kind = FFTW_REDFT10
-    cdef fftw_plan fftplan = fftwf_plan_r2r(1, &dims[0], &plan_arr[0], &plan_arr[0], &kind, 8)
+    cdef fftw_plan fftplan = fftwf_plan_r2r(1, &dims[0], &plan_arr[0], &plan_arr[0], &kind, FFTW_ESTIMATE)
 
+#     print fftplan
 
     fftwf_execute_r2r(fftplan, &X[0], &X[0])
 
